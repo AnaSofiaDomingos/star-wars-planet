94a65599be2bfb7cb6393ea8c820e99065c2a53b
Domingos Ana <ana.domingos@hotmail.fr>
2019-02-28 19:50:19 +0100

    fix : change planet to a class component

--- src/Components/Planets.js
+++ src/Components/Planets.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import './Planets.css'
@@ -7,30 +7,38 @@ import './Planets.css'
  * Display a table with all planets 
  * @param {*} planets : list of planets passed in parameters from the parent component
  */
-const Planets = ({ planets, onClick }) => (
-    <table className="table">
-        <thead className="thead-dark"> 
-            <tr>
-                <th>Name</th>
-                <th>Diameter</th>
-                <th>Terrain</th>
-                <th>Population</th>
-            </tr>
-        </thead>
-        <tbody>
-            {
-                planets.map(({name, diameter, terrain, population, url }) => (
-                    <tr key={name} onClick={() => onClick(name, url)}>
-                        <td className="name">{name}</td>
-                        <td className="diameter">{diameter}</td>
-                        <td className="terrain">{terrain}</td>
-                        <td className="population">{population}</td>
+class Planets extends Component {
+
+
+    render() {
+        console.log(this.props.planets)
+
+        return (
+            <table className="table">
+                <thead className="thead-dark">
+                    <tr>
+                        <th>Name</th>
+                        <th>Diameter</th>
+                        <th>Terrain</th>
+                        <th>Population</th>
                     </tr>
-                ))
-            }
-        </tbody>
-    </table>
-)
+                </thead>
+                <tbody>
+                    {
+                        this.props.planets.map(({ name, diameter, terrain, population, url }) => (
+                            <tr key={name} onClick={() => this.props.onClick(name, url)}>
+                                <td className="name">{name}</td>
+                                <td className="diameter">{diameter}</td>
+                                <td className="terrain">{terrain}</td>
+                                <td className="population">{population}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        )
+    }
+}
 
 // check prop types
 Planets.propTypes = {
